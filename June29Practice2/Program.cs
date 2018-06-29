796e53436913c4d92ef795bdb5b96f48ffd9535e
unknown <bgklein@outlook.com>
2018-06-29 11:58:11 -0400

    initial commit

---
+++ June29Practice2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace June29Practice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(remove_char("codingisfun", 1));
+            Console.WriteLine(remove_char("codingisfun", 9));
+            Console.WriteLine(remove_char("codingisfun", 0));
+        }
+        public static string remove_char(string str, int n)
+        {
+            return str.Remove(n, 1);
+        }
+    }
+}

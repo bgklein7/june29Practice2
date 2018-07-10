99dd03fcb2904eae07cbdd75cb2b7308667324b5
unknown <bgklein@outlook.com>
2018-07-10 16:49:21 -0400

    added reverse string

--- June29Practice2/Program.cs
+++ June29Practice2/Program.cs
@@ -9,10 +9,17 @@ namespace June29Practice2
             Console.WriteLine(remove_char("codingisfun", 1));
             Console.WriteLine(remove_char("codingisfun", 9));
             Console.WriteLine(remove_char("codingisfun", 0));
+            Console.WriteLine(RevString("codingisfun"));
         }
         public static string remove_char(string str, int n)
         {
             return str.Remove(n, 1);
+        }
+        public static string RevString(string str)
+        {
+            char[] arr = str.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
         }
     }
 }
